f6f1b6b5c115331b416258c79664ec45e341520e
Jozev <maga.bid09@gmail.com>
2024-04-08 20:27:01 +0300

    first commit

---
+++ OOAID_2LAB/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace OOAID_2LAB
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сортировка вставками");
+            Console.Write("Введите элементы массива: ");
+            string[] parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            int[] array = [.. parts.Select(int.Parse)];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                array[i] = Convert.ToInt32(parts[i]);
+            }
+            
+            Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", InsertionSort(array)));
+
+            Console.ReadLine();
+        }
+        //метод обмена элементов
+        static void Swap(ref int e1, ref int e2)
+        {
+            (e2, e1) = (e1, e2);
+        }
+
+        //сортировка вставками
+        public static int[] InsertionSort(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                var key = array[i];
+                var j = i;
+                while ((j > 0) && (array[j - 1] > key))
+                {
+                    Swap(ref array[j - 1], ref array[j]);
+                    j--;
+                }
+                array[j] = key;
+            }
+            return array;
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using OOAID_2LAB;
+namespace TestProject1;
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var s = new int[2, 12, 214, 21, 4125, 25, 12, 5, 125, 12, 51, 2, 5];
+        //var a = Program.InsertionSort([1231]);
+        bool isSorted = true;
+        
+        Assert.
+    }
+}
